9cda52d92c2605aa1f04ae2885bf340deb1a3340
DATBOI <alexsimons9999@gmail.com>
2019-02-09 15:42:56 -0600

    Added ui component, so far I have not broken anything

--- src/NormandyComponent.kt
+++ src/NormandyComponent.kt
@@ -1,6 +1,8 @@
+import com.intellij.ide.ui.LafManager
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.BaseComponent
 import com.intellij.util.messages.MessageBusConnection
+import javax.swing.UIManager
 
 /**
  * Forged in the flames of battle by alex.
@@ -12,10 +14,16 @@ class NormandyComponent : BaseComponent {
     lateinit var messageBus: MessageBusConnection
     override fun initComponent() {
         messageBus = ApplicationManager.getApplication().messageBus.connect()
+
+        LafManager.getInstance().addLafManagerListener { lafManager ->
+            UIManager.put("ProgressBarUI", NormandyUI::javaClass.name)
+            UIManager.getDefaults().put(NormandyUI::javaClass.name, NormandyUI::javaClass)
+
+        }
     }
 
     override fun disposeComponent() {
-        if(this::messageBus.isInitialized){
+        if (this::messageBus.isInitialized) {
             messageBus.disconnect()
         }
     }
---
+++ src/NormandyUI.kt
@@ -0,0 +1,10 @@
+import javax.swing.plaf.basic.BasicProgressBarUI
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+
+class NormandyUI : BasicProgressBarUI() {
+
+
+}
