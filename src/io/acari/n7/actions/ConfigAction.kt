be51d339d43130e0306d9c63a2df776bfe8511a7
DATBOI <alexsimons9999@gmail.com>
2019-02-14 18:44:55 -0600

    I found a way to put it on the tool bar and such

---
+++ src/io/acari/n7/actions/ConfigAction.kt
@@ -0,0 +1,11 @@
+package io.acari.n7.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ConfigAction : AnAction() {
+  override fun actionPerformed(p0: AnActionEvent) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+}
