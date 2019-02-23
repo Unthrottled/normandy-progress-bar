010fd8898a7f3a6d453a383b84eca531215c15ff
DATBOI <alexsimons9999@gmail.com>
2019-02-22 19:05:32 -0600

    The list never stops growing

--- src/main/kotlin/io/acari/n7/config/ThemeDefaults.kt
+++ src/main/kotlin/io/acari/n7/config/ThemeDefaults.kt
@@ -2,16 +2,8 @@ package io.acari.n7.config
 
 import javax.swing.plaf.ColorUIResource
 
-/**
- * Default colors for Custom theme
- */
-enum class ThemeDefaults {
-  ;
-
-  //    todo: should probably make these actually what they are
-  companion object {
-    val secondaryColor = ColorUIResource(0x000000)
-    val primaryColor = ColorUIResource(0xFFFFFF)
-    val contrail = ColorUIResource(0xEFEFEF)// todo: should be something like set foreground
-  }
+object ThemeDefaults {
+  val secondaryColor = ColorUIResource(0x000000)
+  val primaryColor = ColorUIResource(0xFFFFFF)
+  val contrail = ColorUIResource(0x64D7EF)
 }
