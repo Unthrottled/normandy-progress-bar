20b1989f1a974998dde2eb0869f538b8ac594238
DATBOI <alexsimons9999@gmail.com>
2019-02-23 17:46:25 -0600

    Re-Calibrations are important

--- src/main/kotlin/io/acari/n7/GuidanceSystem.kt
+++ src/main/kotlin/io/acari/n7/GuidanceSystem.kt
@@ -1,14 +1,14 @@
 package io.acari.n7
 
-import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.JBUI.*
 
 const val DEFAULT_VELOCITY_FROM_CITADEL = 2f
 const val DEFAULT_DISTANCE_FROM_CITADEL = 0f
 val iconWidth = NormandyUI.NORMANDY_TO_CITADEL.iconWidth
 
 class GuidanceSystem {
-  private var distanceFromCitadel: Float = 120f
-  private var velocityFromCitadel: Float = -1f
+  private var distanceFromCitadel: Float = DEFAULT_DISTANCE_FROM_CITADEL
+  private var velocityFromCitadel: Float = DEFAULT_VELOCITY_FROM_CITADEL
 
   fun reCalibrate() { //aka Garrus
     velocityFromCitadel = DEFAULT_VELOCITY_FROM_CITADEL
@@ -20,19 +20,21 @@ class GuidanceSystem {
   fun calculateCurrentLocation():(Int, Int, Float) -> NormandyPositionData =
       { componentWidth, _, offset ->
         distanceFromCitadel =
-            if (distanceFromCitadel < -iconWidth) {
-              velocityFromCitadel = DEFAULT_VELOCITY_FROM_CITADEL
-              2f
-            } else if (distanceFromCitadel >= componentWidth + iconWidth) {
-              velocityFromCitadel = -DEFAULT_VELOCITY_FROM_CITADEL
-              componentWidth.toFloat()
-            } else {
-              distanceFromCitadel
+            when {
+              isAtCitadel() -> {
+                velocityFromCitadel = DEFAULT_VELOCITY_FROM_CITADEL //towards Omega away from the Citadel
+                2f
+              }
+              isAtOmega(componentWidth) -> {
+                velocityFromCitadel = -DEFAULT_VELOCITY_FROM_CITADEL //towards the Citadel and away from Omega
+                componentWidth.toFloat()
+              }
+              else -> distanceFromCitadel
             }
 
         distanceFromCitadel += velocityFromCitadel
 
-        val distanceBetweenCitadelAndNormandy = distanceFromCitadel - JBUI.scale(5f)
+        val distanceBetweenCitadelAndNormandy = distanceFromCitadel - scale(5f)
         val headingToCitadel = isHeadingToCitadel()
         val startingX = if (headingToCitadel) { distanceBetweenCitadelAndNormandy + iconWidth } else 2f * offset
         val distanceBetweenNormandyAndOmega = componentWidth - distanceBetweenCitadelAndNormandy
@@ -40,4 +42,8 @@ class GuidanceSystem {
         val positionOfNormandy = if (headingToCitadel) distanceBetweenCitadelAndNormandy else distanceBetweenCitadelAndNormandy - iconWidth
         NormandyPositionData(startingX, lengthOfContrail, positionOfNormandy.toInt())
       }
+
+  private fun isAtOmega(componentWidth: Int) = distanceFromCitadel >= componentWidth + iconWidth
+
+  private fun isAtCitadel() = distanceFromCitadel < -iconWidth
 }
