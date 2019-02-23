b2cb2a875164dc8378b3798f5de7449d42b9b20e
DATBOI <alexsimons9999@gmail.com>
2019-02-23 13:16:16 -0600

    Consuming but not using the extra enhancments from external themes

--- src/main/kotlin/io/acari/n7/theme/ThemeChangedInformation.kt
+++ src/main/kotlin/io/acari/n7/theme/ThemeChangedInformation.kt
@@ -1,18 +1,16 @@
 package io.acari.n7.theme
 
-import java.lang.IllegalArgumentException
-
-data class ThemeChangedInformation(val accentColor: String, val contrastColor: String, val externalTheme: ExternalTheme)
+data class ThemeChangedInformation(val isDark: Boolean, val accentColor: String, val contrastColor: String, val foregroundColor: String, val externalTheme: ExternalTheme)
 
 data class AccentChangedInformation(val accentColor: String, val externalTheme: ExternalTheme)
 
-enum class ExternalTheme(val displayName: String){
+enum class ExternalTheme(val displayName: String) {
   MATERIAL_UI("Material UI"), DOKI_DOKI("Doki-Doki Theme"), NOT_SET("LUL DUNNO");
 
   companion object {
-    fun byName(name: String) = try{
+    fun byName(name: String) = try {
       valueOf(name)
-    } catch (illegalArgumentException: IllegalArgumentException){
+    } catch (illegalArgumentException: IllegalArgumentException) {
       NOT_SET
     }
   }
