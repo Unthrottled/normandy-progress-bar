4299d80b8a5369e7556cec848c1736a94fa0989d
DATBOI <alexsimons9999@gmail.com>
2019-02-24 12:51:51 -0600

    Moving subscription to earlier, to catch dem topic emissions

--- src/main/kotlin/io/acari/n7/integration/NormandyIntegrationComponent.kt
+++ src/main/kotlin/io/acari/n7/integration/NormandyIntegrationComponent.kt
@@ -15,13 +15,35 @@ import io.acari.n7.icon.SvgLoaderHacker
 
 class NormandyIntegrationComponent : BaseComponent {
 
-  private val messageBus: MessageBusConnection = ApplicationManager.getApplication().messageBus.connect()
-
   companion object {
+
+    private val messageBus: MessageBusConnection = ApplicationManager.getApplication().messageBus.connect()
 
     init {
       setSVGColorPatcher()
+      subscribeToTopics()
+
     }
+
+    /**
+     * Subscribes to a bunch of topics to avoid any other plugin from
+     * overriding the svg color patcher. Thankfully the Normandy's color patcher
+     * saves any other color patcher an applies it as well, successfully preserving
+     * any other plugin theme behaviour with a custom color patcher. Current there
+     * can only be one color patcher at a time, I made it to where their can be more than one.
+     *
+     */
+    private fun subscribeToTopics() {
+      messageBus.subscribe(CONFIGURATION_TOPIC, NormandyConfigurationSubscriber { setSVGColorPatcher() })
+
+      messageBus.subscribe(ProgressWindow.TOPIC, ProgressWindow.Listener { setSVGColorPatcher() })
+
+      messageBus.subscribe(CustomBuilderMessageHandler.TOPIC, ExternalThemeListener { setSVGColorPatcher() })
+
+      messageBus.subscribe(AppLifecycleListener.TOPIC, AppLifecycleSubscriber { setSVGColorPatcher() })
+    }
+
+
     /**
      * Enables the ability to color the Normandy and Alliance Icon.
      */
@@ -35,31 +57,11 @@ class NormandyIntegrationComponent : BaseComponent {
 
   override fun initComponent() {
     setSVGColorPatcher()
-    subscribeToTopics()
+    LafManager.getInstance().addLafManagerListener { setSVGColorPatcher() }
   }
 
   override fun disposeComponent() {
     messageBus.disconnect()
-  }
-
-  /**
-   * Subscribes to a bunch of topics to avoid any other plugin from
-   * overriding the svg color patcher. Thankfully the Normandy's color patcher
-   * saves any other color patcher an applies it as well, successfully preserving
-   * any other plugin theme behaviour with a custom color patcher. Current there
-   * can only be one color patcher at a time, I made it to where their can be more than one.
-   *
-   */
-  private fun subscribeToTopics() {
-    messageBus.subscribe(CONFIGURATION_TOPIC, NormandyConfigurationSubscriber { setSVGColorPatcher() })
-
-    messageBus.subscribe(ProgressWindow.TOPIC, ProgressWindow.Listener { setSVGColorPatcher() })
-
-    messageBus.subscribe(CustomBuilderMessageHandler.TOPIC, ExternalThemeListener { setSVGColorPatcher() })
-
-    messageBus.subscribe(AppLifecycleListener.TOPIC, AppLifecycleSubscriber { setSVGColorPatcher() })
-
-    LafManager.getInstance().addLafManagerListener { setSVGColorPatcher() }
   }
 }
 
