8647494f52f387816b1b5734a88dc652572ce780
DATBOI <alexsimons9999@gmail.com>
2019-06-22 12:56:33 -0500

    More painting the Normandy RGB

--- src/main/kotlin/io/acari/n7/theme/NormandyTheme.kt
+++ src/main/kotlin/io/acari/n7/theme/NormandyTheme.kt
@@ -3,7 +3,23 @@ package io.acari.n7.theme
 import com.intellij.ui.ColorUtil
 import java.awt.Color
 
+
 object NormandyTheme {
+  val RED_COLOR_STRING = "F60000"
+  val RED = ColorUtil.fromHex(RED_COLOR_STRING)
+  val ORANGE_COLOR_STRING = "FF8C00"
+  val ORANGE = ColorUtil.fromHex(ORANGE_COLOR_STRING)
+  val YELLOW_COLOR_STRING = "FFEE00"
+  val YELLOW = ColorUtil.fromHex(YELLOW_COLOR_STRING)
+  val GREEN_COLOR_STRING = "4DE94C"
+  val GREEN = ColorUtil.fromHex(GREEN_COLOR_STRING)
+  val BLUE_COLOR_STRING = "3783FF"
+  val BLUE = ColorUtil.fromHex(BLUE_COLOR_STRING)
+  val INDIGO_COLOR_STRING = "4815AA"
+  val INDIGO = ColorUtil.fromHex(INDIGO_COLOR_STRING)
+  val VIOLET_COLOR_STRING = "901DAA"
+  val VIOLET = ColorUtil.fromHex(VIOLET_COLOR_STRING)
+
   private val pastelRainbow = arrayOf({_:Color -> ColorUtil.fromHex("FF9AA2")},
       { ColorUtil.fromHex("FFB7B2")},
       { ColorUtil.fromHex("FFDAC1")},
@@ -18,13 +34,13 @@ object NormandyTheme {
   val contrailColors: Array<(Color) -> Color>
     get() = if (ThemeConfiguration.isRainbowMode) {
       arrayOf(
-          {_:Color -> ColorUtil.fromHex("F60000")},
-          { ColorUtil.fromHex("FF8C00")},
-          { ColorUtil.fromHex("FFEE00")},
-          { ColorUtil.fromHex("4DE94C")},
-          { ColorUtil.fromHex("3783FF")},
-          { ColorUtil.fromHex("4815AA")},
-          { ColorUtil.fromHex("901DAA")}
+          {_:Color -> RED },
+          { ORANGE },
+          { YELLOW },
+          { GREEN },
+          { BLUE },
+          { INDIGO },
+          { VIOLET }
       )
     } else {
       arrayOf(contrailColor, outerContrailColor,
