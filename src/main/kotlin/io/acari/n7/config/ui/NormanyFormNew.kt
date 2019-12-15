3b94c09f1b9e5ff2aa2206e6202bd6c5fc0f351b
DATBOI <alexsimons9999@gmail.com>
2019-12-15 16:10:12 -0600

    Working on new form

---
+++ src/main/kotlin/io/acari/n7/config/ui/NormanyFormNew.kt
@@ -0,0 +1,31 @@
+package io.acari.n7.config.ui
+
+import com.intellij.openapi.options.SearchableConfigurable
+import javax.swing.JComponent
+
+class NormanyFormNew : SearchableConfigurable {
+  companion object {
+    const val ID = "SSV Normandy Settings"
+  }
+
+  override fun isModified(): Boolean {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun getId(): String {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun getDisplayName(): String {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun apply() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun createComponent(): JComponent? {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+}
