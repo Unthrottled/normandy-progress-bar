601d32b721536c87ba07b60646a800e81a2e6af5
DATBOI <alexsimons9999@gmail.com>
2019-12-18 04:22:33 -0600

    Updated the changelog

--- src/main/kotlin/io/acari/n7/integration/AppLifecycleSubscriber.kt
+++ src/main/kotlin/io/acari/n7/integration/AppLifecycleSubscriber.kt
@@ -2,7 +2,6 @@ package io.acari.n7.integration
 
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Ref
 
 class AppLifecycleSubscriber(private val fundy: () -> Unit) : AppLifecycleListener {
 
